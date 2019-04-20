7632dda00354d81fbcc2d0d5b94d32235c3b556d
Neil Berkman <neil@xuku.com>
2019-04-19 20:27:08 -0700

    remove SDK download

--- scripts/gcloudlogin.sh
+++ scripts/gcloudlogin.sh
@@ -1,8 +1,6 @@
 #! /bin/bash
 
-if [ ! -d "$HOME/google-cloud-sdk/bin" ]; then rm -rf $HOME/google-cloud-sdk; curl https://sdk.cloud.google.com | bash; fi
 # Add gcloud to $PATH
-source $HOME/google-cloud-sdk/path.bash.inc
 gcloud version
 # Authentication flow
 echo $GCLOUD_KEY | base64 --decode > gcloud.json
